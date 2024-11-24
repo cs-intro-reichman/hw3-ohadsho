93567b38f82a87e00d6d5496a8c44eda211ee552
hugo boss <ohad.shoshanilevi@post.runi.ac.il>
2024-11-24 22:25:10 +0200

    done

--- Anagram.java
+++ Anagram.java
@@ -1,17 +1,19 @@
 /** Functions for checking if a given string is an anagram. */
 public class Anagram {
 	public static void main(String args[]) {
-		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
-
-		// Tests the preProcess function.
-		System.out.println(preProcess("What? No way!!!"));
 		
-		// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
+		// Tests the isAnagram function.
+		//System.out.println(isAnagram("silent","listen"));  // true
+		//System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
+		//System.out.println(isAnagram("Madam Curie","Radium came")); // true
+		//System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+		
+		// Tests the preProcess function.
+		//System.out.println(preProcess("What? No way!!!"));
+		
+		
+		 //Tests the randomAnagram function.
+		//System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
 		
 		// Performs a stress test of randomAnagram 
 		String str = "1234567";
@@ -24,11 +26,29 @@ public class Anagram {
 			if (!pass) break;
 		}
 		System.out.println(pass ? "test passed" : "test Failed");
+		
 	}  
+	
+	
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
+		if(str1.length() != str2.length())
+		return false;
+
+		for(int i=0 ; i< str1.length() ; i++){
+			for(int j=0 ; j< str2.length() ; j++){
+				if(str1.charAt(i) == str2.charAt(j) ){
+					str2 = str2.replace(str2.charAt(j), ' ');
+
+					str2 = str2.replaceAll("\\s", "");
+				}
+		}
+	}
+
+	if(str2 == "")
+		return true;
+
 		return false;
 	}
 	   
@@ -36,14 +56,30 @@ public class Anagram {
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		str = str.toLowerCase();
+
+		for(int i=0 ; i< str.length() ; i++){
+			if(str.charAt(i)!='a' && str.charAt(i)!='c' &&str.charAt(i)!='v' && str.charAt(i)!='b'&& str.charAt(i)!='s'  &&str.charAt(i)!='d' &&str.charAt(i)!='f'&&str.charAt(i)!='g'&& str.charAt(i)!='h' &&str.charAt(i)!='j'&& str.charAt(i)!='k' &&str.charAt(i)!='l'&&str.charAt(i)!='q' &&str.charAt(i)!='w' &&str.charAt(i)!='e' &&str.charAt(i)!='r' &&str.charAt(i)!='t' &&str.charAt(i)!='y' &&str.charAt(i)!='u' &&str.charAt(i)!='i' &&str.charAt(i)!='o' &&str.charAt(i)!='p' && str.charAt(i)!='n' && str.charAt(i)!='m'&& str.charAt(i)!='i'&& str.charAt(i)!='z'&& str.charAt(i)!='x'){
+				str = str.replace(str.charAt(i), ' ');
+			}
+		}
+		str = str.replaceAll("\\s", "");
+		return str;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		String randomStr ="";
+		int length = str.length();
+		int random= 0;
+
+		for(int i=0 ; i<length ; i++){
+			random =(int) (Math.random() * (str.length()));
+			randomStr = randomStr + str.charAt(random);
+			str = str.replace(str.charAt(random), ' ');
+			str = str.replaceAll("\\s", "");
+		}
+		return randomStr;
 	}
 }
--- LoanCalc.java
+++ LoanCalc.java
@@ -14,6 +14,9 @@ public class LoanCalc {
 		int n = Integer.parseInt(args[2]);
 		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 
+		//System.out.println(endBalance(loan,rate,n,10000));
+		System.out.println(bisectionSolver(loan, rate, n ,epsilon));
+		/* 
 		// Computes the periodical payment using brute force search
 		System.out.print("\nPeriodical payment, using brute force: ");
 		System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
@@ -23,13 +26,17 @@ public class LoanCalc {
 		System.out.print("\nPeriodical payment, using bi-section search: ");
 		System.out.println((int) bisectionSolver(loan, rate, n, epsilon));
 		System.out.println("number of iterations: " + iterationCounter);
+		*/
 	}
 
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		double loan1 = loan;
+		for(int i=0; i<n ; i++){
+			loan1 = (loan1-payment)*(1+ (rate/100));
+		}
+		return loan1;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,8 +45,11 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		double payment =0;
+		while(endBalance(loan,rate,n,payment)>0){
+			payment = payment + epsilon;
+		}
+		return payment;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -48,7 +58,19 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+		double high=loan;
+		double low = 0;
+		double middle = (high + low)/2;
+		while(high-low > epsilon){
+			if((endBalance(loan,rate,n,middle)*endBalance(loan,rate,n,low)>0)){
+				low = middle;
+				middle = (high+low)/2;
+			}
+			else{
+				high = middle;
+				middle = (high+low)/2;
+			}
+		}
+		return middle;
     }
 }
