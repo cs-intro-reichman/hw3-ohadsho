6f874e0d4d5521f4149dee45d99090a60c9bd5ce
hugo boss <ohad.shoshanilevi@post.runi.ac.il>
2024-11-25 11:13:35 +0200

    done

--- Anagram.java
+++ Anagram.java
@@ -52,7 +52,6 @@ public class Anagram {
 
 		    strTemp = str2;
 			str2 = strTemp.substring(0,j) + strTemp.substring(j +1, strTemp.length());
-			System.out.println(str2);
 				}
 		}
 	}
@@ -68,11 +67,12 @@ public class Anagram {
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
 		str = str.toLowerCase();
+		String strTemp =str;
 
 		for(int i=0 ; i< str.length() ; i++){
-			if(str.charAt(i)!='a' && str.charAt(i)!='c' &&str.charAt(i)!='v' && str.charAt(i)!='b'&& str.charAt(i)!='s'  &&str.charAt(i)!='d' &&str.charAt(i)!='f'&&str.charAt(i)!='g'&& str.charAt(i)!='h' &&str.charAt(i)!='j'&& str.charAt(i)!='k' &&str.charAt(i)!='l'&&str.charAt(i)!='q' &&str.charAt(i)!='w' &&str.charAt(i)!='e' &&str.charAt(i)!='r' &&str.charAt(i)!='t' &&str.charAt(i)!='y' &&str.charAt(i)!='u' &&str.charAt(i)!='i' &&str.charAt(i)!='o' &&str.charAt(i)!='p' && str.charAt(i)!='n' && str.charAt(i)!='m'&& str.charAt(i)!='i'&& str.charAt(i)!='z'&& str.charAt(i)!='x'){
-				str = str.replace(str.charAt(i), ' ');
-			}
+			if(str.charAt(i)!='a' && str.charAt(i)!='c' &&str.charAt(i)!='v' && str.charAt(i)!='b'&& str.charAt(i)!='s'  &&str.charAt(i)!='d' &&str.charAt(i)!='f'&&str.charAt(i)!='g'&& str.charAt(i)!='h' &&str.charAt(i)!='j'&& str.charAt(i)!='k' &&str.charAt(i)!='l'&&str.charAt(i)!='q' &&str.charAt(i)!='w' &&str.charAt(i)!='e' &&str.charAt(i)!='r' &&str.charAt(i)!='t' &&str.charAt(i)!='y' &&str.charAt(i)!='u' &&str.charAt(i)!='i' &&str.charAt(i)!='o' &&str.charAt(i)!='p' && str.charAt(i)!='n' && str.charAt(i)!='m'&& str.charAt(i)!='i'&& str.charAt(i)!='z'&& str.charAt(i)!='x' && str.charAt(i)!=' '){
+				strTemp = str;
+				str = strTemp.substring(0,i) + strTemp.substring(i +1, strTemp.length());			}
 		}
 		return str;
 	} 
